47768b10e7cac084a557b80847e7ff8ceedeb666
zengty-2018 <213432534@qew.com>
2020-12-21 15:23:50 +0800

    update2

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index'
+import ArticleDetails from '@/components/ArticleDetails'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/article/details/:id',
+      name: 'article details',
+      component: ArticleDetails
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/join',
+      name: 'sign up',
+      component: Signup
     }
-  ]
+  ],
+  mode: 'history'
 })
